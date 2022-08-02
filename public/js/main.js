6d56b13c86e8c367dce58a461e7204f3d301825e
shinopai <harbor.shinozaki@gmail.com>
2022-08-02 19:05:42 +0900

    finished up to recipe section for sp design

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,31 @@ $(function () {
     dots: true,
     pauseOnDotsHover: false,
   });
+
+  // sp menu
+  let menuIcon = $("#menu_icon");
+  let spMenu = $("#sp_menu");
+  let closeButton = $("#close_button");
+
+  menuIcon.click(function () {
+    spMenu.addClass("active");
+    spMenu.animate(
+      {
+        top: 0,
+      },
+      500
+    );
+    menuIcon.addClass("inactive");
+  });
+
+  closeButton.click(function () {
+    spMenu.animate(
+      {
+        top: -354,
+      },
+      500
+    );
+    // spMenu.removeClass("active");
+    menuIcon.removeClass("inactive");
+  });
 });
